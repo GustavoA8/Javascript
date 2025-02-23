8e1944a072f1cbb7b88592fdfed6a0854a42dc1b
Gustavo <fefe.giulia2@gmail.com>
2025-02-23 18:15:28 -0300

    sla

--- tabela_imc/assets/js/main.js
+++ tabela_imc/assets/js/main.js
@@ -7,6 +7,9 @@ function meuEscopo() {
         const peso = document.getElementById("peso")
         const altura = document.getElementById("altura")
         const imc = peso.value / (altura.value*altura.value)
+        const resultado = document.getElementById("resultado")
+
+        console.log(imc)
 
 
 
@@ -18,21 +21,27 @@ function meuEscopo() {
 
         if (imc < 18.5) {
             console.log(1)
+            resultado.innerHTML = "<p>Você esta Abaixo do Peso<\p>";
 
         } else if (imc <= 24.9) {
             console.log(2)
+            resultado.innerHTML = "<p>Você esta com o Peso Normal<\p>";
 
         } else if (imc <= 29.9) {
             console.log(3)
+            resultado.innerHTML = "<p>Você esta com Sobrepeso<\p>";
 
         } else if (imc <= 34.9) {
             console.log(4)
+            resultado.innerHTML = "<p>Você esta com Obesidade Grau I<\p>";
 
         } else if (imc <= 39.9) {
             console.log(5)
+            resultado.innerHTML = "<p>Você esta com Obesidade Grau II<\p>";
 
         } else {
             console.log(6)
+            resultado.innerHTML = "<p>Você esta com Obesidade Grau III<\p>";
 
         }
 
